da521bb59dd156779a05646c4defb98e285f36a4
shoshin-programmer <atienza.new@gmail.com>
2020-12-21 17:52:47 +0800

    dynamic loading

---
+++ pages/blog/[id].js
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+import posts from "../../posts.json";
+
+export default () => {
+  const router = useRouter();
+
+  const post = posts[router.query.id];
+
+  return (
+    <div className="container">
+      <main className="main">
+        {post ? (
+          <>
+            <h2>{post.title}</h2>
+            <small>{post.content}</small>
+          </>
+        ) : (
+          <>
+            <h2>Preloading</h2>
+            <small>This will show while the data is being fetched.</small>
+          </>
+        )}
+      </main>
+    </div>
+  );
+};
